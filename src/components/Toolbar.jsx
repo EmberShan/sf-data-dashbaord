1396091bb2bfda59b69f968df7f653fc0f3378bf
Ember Shan <szixin@umich.edu>
2025-06-05 16:44:07 -0400

    --

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -34,37 +34,9 @@ const Toolbar = ({ onCreateChart, onPrintPage }) => {
       <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Left: Avatar */}
-          <div className="flex">
-            <div
-              onClick={onCreateChart}
-              className="px-3 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2"
-            >
-              <img src="/add.svg" alt="Add" className="w-6 h-6" />
-              Create Chart
-            </div>
-            <div
-              onClick={handleDownloadPDF}
-              className="px-2 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2 min-w-[180px]"
-            >
-              {isLoadingPDF ? (
-                <span className="w-6 h-6 inline-block animate-spin border-2 border-gray-300 border-t-[#3398FF] rounded-full"></span>
-              ) : (
-                <span className="w-6 h-6">
-                  {" "}
-                  <img src="/download.svg" alt="Download" className="w-full" />
-                </span>
-              )}
-              Download Report PDF
-            </div>
-            <div
-              //   onClick={() => alert('Share placeholder')}
-              className="px-3 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2"
-            >
-              <span className="w-6 h-6">
-                {" "}
-                <img src="/share.svg" alt="Download" className="w-full" />
-              </span>
-              Share
+          <div className="flex items-center">
+            <div className="w-8 h-8 rounded-full border border-gray-400 text-gray-700 font-semibold flex items-center justify-center">
+              E
             </div>
           </div>
 
@@ -103,9 +75,37 @@ const Toolbar = ({ onCreateChart, onPrintPage }) => {
           </nav>
 
           {/* Right: Create Chart + Share + Print */}
-          <div className="flex items-center">
-            <div className="w-8 h-8 rounded-full border border-gray-400 text-gray-700 font-semibold flex items-center justify-center">
-              E
+          <div className="flex">
+            <div
+              onClick={onCreateChart}
+              className="px-3 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2"
+            >
+              <img src="/add.svg" alt="Add" className="w-6 h-6" />
+              Create Chart
+            </div>
+            <div
+              onClick={handleDownloadPDF}
+              className="px-2 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2 min-w-[180px]"
+            >
+              {isLoadingPDF ? (
+                <span className="w-6 h-6 inline-block animate-spin border-2 border-gray-300 border-t-[#3398FF] rounded-full"></span>
+              ) : (
+                <span className="w-6 h-6">
+                  {" "}
+                  <img src="/download.svg" alt="Download" className="w-full" />
+                </span>
+              )}
+              Download Report PDF
+            </div>
+            <div
+              //   onClick={() => alert('Share placeholder')}
+              className="px-3 py-1.5 text-sm rounded text-gray-600 hover:text-[#3398FF] cursor-pointer flex items-center gap-2"
+            >
+              <span className="w-6 h-6">
+                {" "}
+                <img src="/share.svg" alt="Download" className="w-full" />
+              </span>
+              Share
             </div>
           </div>
         </div>
